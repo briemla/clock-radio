8f403ef4b5a1a49ccfcc3c6efd432e5a9869287c
Lars <briem.lars@googlemail.com>
2014-12-10 00:16:27 +0100

    Replaced pointer with circle and line. Position of circle and line end will be changed.

--- clock-radio/src/main/java/de/briemla/clockradio/controls/Time.java
+++ clock-radio/src/main/java/de/briemla/clockradio/controls/Time.java
@@ -3,12 +3,13 @@ package de.briemla.clockradio.controls;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Translate;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import jfxtras.scene.layout.CircularPane;
 import de.briemla.clockradio.FXUtil;
 
@@ -28,8 +29,12 @@ public class Time extends AnchorPane {
 	private CircularPane afternoon;
 	@FXML
 	private CircularPane minuteCircle;
+	// @FXML
+	// private Pointer pointer;
 	@FXML
-	private Pointer pointer;
+	private Circle circle;
+	@FXML
+	private Line line;
 
 	private final SimpleIntegerProperty hourProperty;
 	private final SimpleIntegerProperty minuteProperty;
@@ -38,8 +43,8 @@ public class Time extends AnchorPane {
 		super();
 		FXUtil.load(this, this);
 
-		hourProperty = new SimpleIntegerProperty();
-		minuteProperty = new SimpleIntegerProperty();
+		hourProperty = new SimpleIntegerProperty(0);
+		minuteProperty = new SimpleIntegerProperty(0);
 		for (int currentHour = 0; currentHour <= 11; currentHour++) {
 			morning.add(timeLabelFor(currentHour));
 		}
@@ -50,21 +55,21 @@ public class Time extends AnchorPane {
 			minuteCircle.add(timeLabelFor(currentMinute * 5 % 60));
 		}
 
+		line.startXProperty().bind(hourParent.widthProperty().divide(2d));
+		line.startYProperty().bind(hourParent.heightProperty().divide(2d));
+		hourProperty.addListener(change -> updateHourPointer());
+		hourParent.widthProperty().addListener(change -> updateHourPointer());
+		hourParent.heightProperty().addListener(change -> updateHourPointer());
+
 		hourParent.addEventHandler(MouseEvent.ANY, event -> {
 			if (event.isPrimaryButtonDown()) {
-				hourProperty.set(Angle.toHour(event.getX(), event.getY(), hourParent.getWidth(), hourParent.getHeight()));
+				double x = event.getX();
+				double y = event.getY();
+				double width = hourParent.getWidth();
+				double height = hourParent.getHeight();
+				hourProperty.set(Angle.toHour(x, y, width, height));
 				event.consume();
 			}
-		});
-
-		double translateX = 150d - 10d;
-		double translateY = 55d;
-		pointer.getTransforms().add(new Translate(translateX, translateY));
-		hourProperty.addListener((change, oldValue, newValue) -> {
-			pointer.getTransforms().add(new Translate(-translateX, -translateY));
-			pointer.getTransforms().add(new Rotate((int) oldValue * -30d, 150d, 150d));
-			pointer.getTransforms().add(new Rotate((int) newValue * 30d, 150d, 150d));
-			pointer.getTransforms().add(new Translate(translateX, translateY));
 		});
 
 		hour.textProperty().bind(hourProperty.asString("%02d"));
@@ -72,6 +77,41 @@ public class Time extends AnchorPane {
 
 		hour.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> switchToHour());
 		minute.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> switchToMinute());
+	}
+
+	private void updateHourPointer() {
+		double width = hourParent.getWidth();
+		double height = hourParent.getHeight();
+		double centerX = width / 2.0d;
+		double centerY = height / 2.0d;
+		Point2D center = new Point2D(centerX, centerY);
+		int toHour = hourProperty.get();
+		double distanceToCenter = distanceTo(toHour);
+		Point2D currentHourAligned = align(center, toHour);
+		Point2D newCoordinates = toCircle(currentHourAligned, center, distanceToCenter);
+		circle.setCenterX(newCoordinates.getX());
+		circle.setCenterY(newCoordinates.getY());
+		line.setEndX(newCoordinates.getX());
+		line.setEndY(newCoordinates.getY());
+	}
+
+	private static Point2D align(Point2D center, int currentHour) {
+		double hourAlignedX = center.getX() + Math.cos(Math.toRadians(currentHour * 30d - 90d));
+		double hourAlignedY = center.getY() + Math.sin(Math.toRadians(currentHour * 30d - 90d));
+		return new Point2D(hourAlignedX, hourAlignedY);
+	}
+
+	private double distanceTo(int toHour) {
+		double morningDistance = morning.getDiameter() / 2d;
+		double afternoonDistance = afternoon.getDiameter() / 2d;
+		return (toHour >= 12 ? afternoonDistance : morningDistance) - 10d;
+	}
+
+	private static Point2D toCircle(Point2D current, Point2D center, double suggestedLength) {
+		Point2D relative = current.subtract(center);
+		double distance = relative.magnitude();
+		double factor = distance == 0d ? suggestedLength : suggestedLength / distance;
+		return relative.multiply(factor).add(center);
 	}
 
 	private static Label timeLabelFor(int time) {
