c274c1873cdc6d8eba466ed417d5a4bac9139682
Lars <briem.lars@googlemail.com>
2015-01-13 21:07:48 +0100

    Added comment

--- clock-radio/src/main/java/de/briemla/clockradio/Alarm.java
+++ clock-radio/src/main/java/de/briemla/clockradio/Alarm.java
@@ -50,6 +50,24 @@ public class Alarm {
 
 		private void startTimer() {
 			startTimer = new Timer("AlarmTimer", true);
+			// TODO: Maybe use Timer#schedule to run the timer on specified dates:
+			// Timer timer new Timer();
+			// Thread myThread= // Your thread
+			// Calendar date = Calendar.getInstance();
+			// date.set(
+			// Calendar.DAY_OF_WEEK,
+			// Calendar.SUNDAY
+			// );
+			// date.set(Calendar.HOUR, 0);
+			// date.set(Calendar.MINUTE, 0);
+			// date.set(Calendar.SECOND, 0);
+			// date.set(Calendar.MILLISECOND, 0);
+			// // Schedule to run every Sunday in midnight
+			// timer.schedule(
+			// new SampleTask (myThread),
+			// date.getTime(),
+			// 1000 * 60 * 60 * 24 * 7
+			// );
 			startTimer.scheduleAtFixedRate(new AlarmTask(alarm), alarm.alarmDate(), Long.MAX_VALUE);
 			stopTimer = new Timer("AlarmStopTimer", true);
 			stopTimer.scheduleAtFixedRate(new AlarmStopTask(alarm), alarm.alarmStopDate(), Long.MAX_VALUE);
@@ -163,8 +181,7 @@ public class Alarm {
 
 	private LocalDateTime alarmLocalDate() {
 		LocalDateTime date = LocalDateTime.now();
-		if (date.getHour() > hourProperty.get() || date.getHour() == hourProperty.get()
-				&& date.getMinute() >= minuteProperty.get()) {
+		if (date.getHour() > hourProperty.get() || date.getHour() == hourProperty.get() && date.getMinute() >= minuteProperty.get()) {
 			date = date.plusDays(1);
 		}
 		date = date.withHour(hourProperty.get());
