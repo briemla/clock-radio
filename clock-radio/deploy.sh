2727fd176f8184b21505cc4dd508fd80da1b7817
lars <briem.lars@googlemail.com>
2015-03-15 14:11:49 +0100

    Cancel after build error

--- clock-radio/deploy.sh
+++ clock-radio/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "Pulling changes from git"
 git pull
-mvn clean compile assembly:single
+
+echo "Building clock-radio using maven"
+mvn clean compile assembly:single && { echo 'Successfully build clock-radio ; } || { echo 'Building clock-radio failed' ; exit 1; }
+echo "Deploying artifacts to clock-radio.local"
 scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar pi@clock-radio.local:/opt/clock-radio/
  
