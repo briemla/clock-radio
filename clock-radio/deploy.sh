8a73dd4c82870d4a94a6f10e47e78128c921ce81
lars <briem.lars@googlemail.com>
2015-03-15 14:13:00 +0100

    Fixed missing quote

--- clock-radio/deploy.sh
+++ clock-radio/deploy.sh
@@ -3,7 +3,7 @@ echo "Pulling changes from git"
 git pull
 
 echo "Building clock-radio using maven"
-mvn clean compile assembly:single && { echo 'Successfully build clock-radio ; } || { echo 'Building clock-radio failed' ; exit 1; }
+mvn clean compile assembly:single && { echo 'Successfully build clock-radio' ; } || { echo 'Building clock-radio failed' ; exit 1; }
 echo "Deploying artifacts to clock-radio.local"
 scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar pi@clock-radio.local:/opt/clock-radio/
  
