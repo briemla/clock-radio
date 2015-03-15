124d235b04629696cb7041aeb61f55981f3d4dbc
Lars <briem.lars@googlemail.com>
2015-03-15 13:57:01 +0100

    Show popup

--- clock-radio/src/main/java/de/briemla/clockradio/Settings.java
+++ clock-radio/src/main/java/de/briemla/clockradio/Settings.java
@@ -6,6 +6,7 @@ import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.stage.Popup;
 
 public class Settings {
 
@@ -54,10 +55,16 @@ public class Settings {
 	}
 
 	public void select(Alarm alarm) {
-		AlarmMenu menu = overlaySwitcher.show(Alarm.class);
-		menu.setCurrentAlarm(alarm);
+		// AlarmMenu menu = overlaySwitcher.show(Alarm.class);
+		// menu.setCurrentAlarm(alarm);
+		AlarmMenu alarmMenu = new AlarmMenu();
+		alarmMenu.setCurrentAlarm(alarm);
+		Popup popup = new Popup();
+		popup.setAutoHide(true);
+		popup.getContent().add(alarmMenu);
 		AlarmSettings alarmSettings = viewSwitcher.show(Alarm.class);
 		alarmSettings.setCurrentAlarm(alarm);
+		popup.show(alarmSettings.getScene().getWindow());
 	}
 
 	public void delete(Alarm alarm) {
