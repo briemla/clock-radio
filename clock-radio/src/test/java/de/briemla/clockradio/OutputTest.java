1e2bde5e9c6c92252cae96d2c4494ef64ec9d442
Lars <briem.lars@googlemail.com>
2015-03-30 20:50:36 +0200

    Added test for output and empty error result

---
+++ clock-radio/src/test/java/de/briemla/clockradio/OutputTest.java
@@ -0,0 +1,16 @@
+package de.briemla.clockradio;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class OutputTest {
+
+	@Test
+	public void emptyError() throws Exception {
+		Output output = new Output();
+
+		assertThat(output.isErrorEmpty(), is(true));
+	}
+}
