5de9cf8d103e6c0111acf8fbacee000f92afaeb9
Lars <briem.lars@googlemail.com>
2015-04-10 11:25:39 +0200

    test serialize method and remove useless test

--- clock-radio/src/test/java/de/briemla/clockradio/dabpi/command/SwitchToDABTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/dabpi/command/SwitchToDABTest.java
@@ -1,28 +1,18 @@
 package de.briemla.clockradio.dabpi.command;
 
-import java.io.IOException;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-import de.briemla.clockradio.Output;
-
 public class SwitchToDABTest {
 
-	@Test(expected = IOException.class)
-	public void parseError() throws Exception {
-		Output output = new Output();
-		output.addError("something");
-
-		SwitchToDAB switchToDAB = new SwitchToDAB();
-		switchToDAB.parse(output);
-	}
-
 	@Test
-	public void parseEmptyError() throws Exception {
-		Output output = new Output();
+	public void serialize() throws Exception {
+		SwitchToDAB command = new SwitchToDAB();
+		String serialized = command.serialize();
 
-		SwitchToDAB switchToDAB = new SwitchToDAB();
-		switchToDAB.parse(output);
+		assertThat(serialized, is(equalTo(" -a")));
 	}
-
 }
