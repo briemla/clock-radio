f9ffb2c89ebfe82f1c80db07ed5a1c704a743e56
lars <briem.lars@googlemail.com>
2015-04-14 17:11:23 +0200

    restore station tests

--- clock-radio/src/test/java/de/briemla/clockradio/dabpi/DABStationTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/dabpi/DABStationTest.java
@@ -1,1 +1,36 @@
 package de.briemla.clockradio.dabpi;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Test;
+
+import de.briemla.clockradio.dabpi.command.Region;
+import de.briemla.clockradio.dabpi.result.DABChannel;
+import de.briemla.clockradio.dabpi.result.DABService;
+
+public class DABStationTest {
+
+	@Test
+	public void tuneTo() throws Exception {
+		RadioController controller = mock(RadioController.class);
+		Region region = Region.BADEN_WUERTEMBERG;
+		DABService service = new DABService(1, "d3d0", "Some name");
+		DABChannel channel = new DABChannel(2);
+		DABStation station = new DABStation(region, service, channel);
+
+		station.tuneTo(controller);
+
+		verify(controller).switchToDAB();
+		verify(controller).selectDABRegion(region);
+		verify(controller).selectDABChannel(channel);
+		verify(controller).startDABService(service);
+	}
+
+	@Test
+	public void equalsAndHashCode() throws Exception {
+		EqualsVerifier.forClass(DABStation.class).allFieldsShouldBeUsed().usingGetClass().verify();
+	}
+
+}
--- clock-radio/src/test/java/de/briemla/clockradio/dabpi/FMStationTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/dabpi/FMStationTest.java
@@ -1,1 +1,41 @@
 package de.briemla.clockradio.dabpi;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Test;
+
+public class FMStationTest {
+
+	@Test
+	public void tuneTo() throws Exception {
+		Integer frequency = 105500;
+		FMStation station = new FMStation(frequency);
+		RadioController controller = mock(RadioController.class);
+
+		station.tuneTo(controller);
+
+		verify(controller).switchToFM();
+		verify(controller).tuneTo(frequency);
+	}
+
+	@Test
+	public void compareTo() throws Exception {
+		FMStation lowerStation = new FMStation(1);
+		FMStation higherStation = new FMStation(2);
+
+		assertThat(lowerStation, is(lessThan(higherStation)));
+		assertThat(higherStation, is(greaterThan(lowerStation)));
+	}
+
+	@Test
+	public void equalsAndHashCode() throws Exception {
+		EqualsVerifier.forClass(FMStation.class).allFieldsShouldBeUsed().usingGetClass().verify();
+	}
+
+}
