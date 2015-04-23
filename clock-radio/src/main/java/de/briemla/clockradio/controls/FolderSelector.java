a65317c356e72bb91e5cd8fae5e3049ced026236
Lars <briem.lars@googlemail.com>
2015-04-23 21:11:48 +0200

    remove unnecessary code

--- clock-radio/src/main/java/de/briemla/clockradio/controls/FolderSelector.java
+++ clock-radio/src/main/java/de/briemla/clockradio/controls/FolderSelector.java
@@ -6,12 +6,10 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import de.briemla.clockradio.FXUtil;
 import de.briemla.clockradio.Media;
 
@@ -51,25 +49,4 @@ public class FolderSelector extends VBox {
 		return mediaProperty;
 	}
 
-	@FXML
-	public void browse(ActionEvent event) {
-		FileChooser fileChooser = new FileChooser();
-		File currentFolder = currentFolder();
-		if (currentFolder.exists()) {
-			fileChooser.setInitialDirectory(currentFolder);
-		}
-		File newSource = fileChooser.showOpenDialog(getScene().getWindow());
-		if (newSource == null) {
-			return;
-		}
-		mediaProperty.set(new LocalFolder(newSource.toPath()));
-	}
-
-	private File currentFolder() {
-		if (mediaProperty.get() instanceof LocalFolder) {
-			File currentSource = ((LocalFolder) mediaProperty.get()).getSource();
-			return currentSource.isDirectory() ? currentSource : currentSource.getParentFile();
-		}
-		return new File("");
-	}
 }
