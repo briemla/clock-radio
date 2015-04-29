88a64c7c866850c32e1e2cd9279b052c5114d5f1
Lars <briem.lars@googlemail.com>
2015-04-29 10:02:49 +0200

    fix wrong directory name

--- clock-radio/deploy.sh
+++ clock-radio/deploy.sh
@@ -5,5 +5,5 @@ git pull
 echo "Building clock-radio using maven"
 mvn clean compile assembly:single && { echo 'Successfully build clock-radio' ; } || { echo 'Building clock-radio failed' ; exit 1; }
 echo "Deploying artifacts to clock-radio.local"
-scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar clock-radio:/home/pi/dabpi_clt/
+scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar clock-radio:/home/pi/dabpi_ctl/
  
