fe9e2d197b1360721afd28842e817e135d762130
Lars Briem <briem.lars@googlemail.com>
2016-02-18 22:26:24 +0100

    Replace CheckBox with ToggleButton.
    Background on CheckBox was not displayed correctly.

--- clock-radio/src/main/java/de/briemla/clockradio/controls/AlarmCell.java
+++ clock-radio/src/main/java/de/briemla/clockradio/controls/AlarmCell.java
@@ -3,9 +3,10 @@ package de.briemla.clockradio.controls;
 import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
+
 import de.briemla.clockradio.Alarm;
 import de.briemla.clockradio.FXUtil;
 import de.briemla.clockradio.Settings;
@@ -15,7 +16,7 @@ public class AlarmCell extends AnchorPane {
     @FXML
     private AnchorPane container;
     @FXML
-    private CheckBox active;
+    private ToggleButton active;
     @FXML
     private Label time;
     @FXML
