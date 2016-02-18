59926875b96c2d0ca69e4572eed4d05a86f3ec65
Lars Briem <briem.lars@googlemail.com>
2016-02-18 21:10:00 +0100

    Rename FxmlTemplateLoader. Fix comments.

--- clock-radio/src/main/java/de/briemla/clockradio/FXUtil.java
+++ clock-radio/src/main/java/de/briemla/clockradio/FXUtil.java
@@ -5,23 +5,24 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 
 import javafx.fxml.FXMLLoader;
-import de.briemla.fxmltemplateloader.FXMLTemplateLoader;
-import de.briemla.fxmltemplateloader.ITemplate;
+
+import de.briemla.fxmltemplateloader.FxmlTemplateLoader;
+import de.briemla.fxmltemplateloader.template.ITemplate;
 
 public class FXUtil {
 
     private static final String FXML_EXTENSION = ".fxml";
 
     /**
-     * Loads a FXML file with the same name as the controller. IOExceptions will
-     * be thrown as RuntimeExceptions to keep client code clean.
+     * Loads a FXML file with the same name as the controller. IOExceptions will be thrown as
+     * RuntimeExceptions to keep client code clean.
      *
      * @param controller
      * @return
      */
     public static <T> T load(Object controller) {
         URL resource = findResource(controller);
-        FXMLTemplateLoader loader = new FXMLTemplateLoader();
+        FxmlTemplateLoader loader = new FxmlTemplateLoader();
         loader.setController(controller);
         try {
             return loader.doLoad(resource);
@@ -37,8 +38,8 @@ public class FXUtil {
     }
 
     /**
-     * Loads a FXML file with the same name as the controller. IOExceptions will
-     * be thrown as RuntimeExceptions to keep client code clean.
+     * Loads a FXML file with the same name as the controller. IOExceptions will be thrown as
+     * RuntimeExceptions to keep client code clean.
      *
      * @param controller
      * @param root
@@ -46,20 +47,30 @@ public class FXUtil {
      */
     public static <T> T load(Object controller, Object root) {
         URL resource = findResource(controller);
-        return loadWithFXMLLoader(controller, root, resource);
-        // return loadWithFXMLTemplateLoader(controller, root, resource);
+        // return loadWithFXMLLoader(controller, root, resource);
+        return loadWithFXMLTemplateLoader(controller, root, resource);
     }
 
     private static <T> T loadWithFXMLTemplateLoader(Object controller, Object root, URL resource) {
         try {
-            ITemplate loadedTemplate = FXMLTemplateLoader.loadTemplate(resource);
+            ITemplate loadedTemplate = FxmlTemplateLoader.loadTemplate(resource);
             loadedTemplate.setRoot(root);
             return loadedTemplate.create(controller);
-        } catch (IOException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException exception) {
+        } catch (IOException | InstantiationException | IllegalAccessException
+                | IllegalArgumentException | InvocationTargetException exception) {
             throw new RuntimeException(exception);
         }
     }
 
+    /**
+     * Just to test against the original FXMLLoader
+     *
+     * @param controller
+     * @param root
+     * @param resource
+     * @return
+     */
+    @SuppressWarnings("unused")
     private static <T> T loadWithFXMLLoader(Object controller, Object root, URL resource) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
