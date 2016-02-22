9c5bcee77c1969551abcc79fa71ebe4c5b86fadf
Lars Briem <briem.lars@googlemail.com>
2016-02-22 19:35:47 +0100

    Revert to checkbox.

--- clock-radio/src/main/java/de/briemla/clockradio/controls/AlarmCell.java
+++ clock-radio/src/main/java/de/briemla/clockradio/controls/AlarmCell.java
@@ -3,8 +3,8 @@ package de.briemla.clockradio.controls;
 import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 
 import de.briemla.clockradio.Alarm;
@@ -16,7 +16,7 @@ public class AlarmCell extends AnchorPane {
     @FXML
     private AnchorPane container;
     @FXML
-    private ToggleButton active;
+    private CheckBox active;
     @FXML
     private Label time;
     @FXML
