303afb5e67333f01b4c68775d08148c95adeec85
Lars Briem <briem.lars@googlemail.com>
2016-04-07 20:58:31 +0200

    Refactor

--- clock-radio/src/test/java/de/briemla/clockradio/AlarmTriggerTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/AlarmTriggerTest.java
@@ -6,18 +6,25 @@ import static org.mockito.Mockito.verify;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.briemla.clockradio.controls.Trigger;
 
 public class AlarmTriggerTest {
 
+    private ObservableList<Alarm> alarms;
+    private Trigger trigger;
+
+    @Before
+    public void initialise() {
+        alarms = FXCollections.observableArrayList();
+        trigger = new AlarmTrigger();
+        trigger.bind(alarms);
+    }
+
     @Test
     public void startOneActiveAlarm() throws Exception {
-        ObservableList<Alarm> alarms = FXCollections.observableArrayList();
-        Trigger trigger = new AlarmTrigger();
-        trigger.bind(alarms);
-
         Alarm alarm = mock(Alarm.class);
         alarms.add(alarm);
 
