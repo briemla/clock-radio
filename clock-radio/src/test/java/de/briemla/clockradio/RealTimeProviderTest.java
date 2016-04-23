db47cdf47d868183125a5d45f86b6d75062af99f
Lars Briem <briem.lars@googlemail.com>
2016-04-23 15:21:40 +0200

    Add test cases

---
+++ clock-radio/src/test/java/de/briemla/clockradio/RealTimeProviderTest.java
@@ -0,0 +1,110 @@
+package de.briemla.clockradio;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.isHour;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.isMinute;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.isSecond;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.sameDay;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.sameMonthOfYear;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.sameOrAfter;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.sameOrBefore;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.sameYear;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAmount;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RealTimeProviderTest {
+
+    private static final TemporalAmount timeCorridor = Duration.of(2, MINUTES);
+    private TimeProvider timeProvider;
+
+    @Before
+    public void initialise() {
+        timeProvider = new RealTimeProvider();
+    }
+
+    @Test
+    public void now() throws Exception {
+        LocalDateTime before = LocalDateTime.now();
+        LocalDateTime now = timeProvider.now();
+        LocalDateTime after = LocalDateTime.now();
+
+        assertThat(now, is(sameOrAfter(before)));
+        assertThat(now, is(sameOrBefore(after)));
+    }
+
+    @Test
+    public void today() throws Exception {
+        LocalDateTime today = LocalDateTime.now();
+
+        LocalDateTime provided = timeProvider.today();
+
+        assertThat(provided, sameDay(today));
+        assertThat(provided, sameMonthOfYear(today));
+        assertThat(provided, sameYear(today));
+        assertThat(provided, isHour(0));
+        assertThat(provided, isMinute(0));
+        assertThat(provided, isSecond(0));
+    }
+
+    @Test
+    public void nextMinute() throws Exception {
+        LocalTime before = LocalTime.now();
+        LocalTime after = LocalTime.now().plus(timeCorridor);
+
+        LocalTime nextMinute = timeProvider.nextMinute();
+
+        assertThat(nextMinute, is(after(before)));
+        assertThat(nextMinute, is(before(after)));
+    }
+
+    private Matcher<LocalTime> after(LocalTime time) {
+        return new TypeSafeMatcher<LocalTime>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is after: " + time);
+            }
+
+            @Override
+            protected boolean matchesSafely(LocalTime item) {
+                return item.isAfter(time);
+            }
+
+            @Override
+            protected void describeMismatchSafely(LocalTime item, Description description) {
+                description.appendText(item + "not after: " + time);
+            }
+        };
+    }
+
+    private Matcher<LocalTime> before(LocalTime time) {
+        return new TypeSafeMatcher<LocalTime>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("before " + time);
+            }
+
+            @Override
+            protected boolean matchesSafely(LocalTime item) {
+                return item.isBefore(time);
+            }
+
+            @Override
+            protected void describeMismatchSafely(LocalTime item, Description description) {
+                description.appendText(item + " is not before " + time);
+            }
+        };
+    }
+}
