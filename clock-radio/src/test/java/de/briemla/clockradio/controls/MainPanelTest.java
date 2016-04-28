55497168308db26024c79941798837f34073a1db
Lars Briem <briem.lars@googlemail.com>
2016-04-28 23:08:47 +0200

    Fix issue 14

--- clock-radio/src/test/java/de/briemla/clockradio/controls/MainPanelTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/controls/MainPanelTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalTime;
@@ -17,10 +16,12 @@ import javafx.scene.Parent;
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 
+import de.briemla.clockradio.Alarm;
 import de.briemla.clockradio.AlarmFactory;
 import de.briemla.clockradio.AlarmStorage;
 import de.briemla.clockradio.TimeProvider;
 import de.briemla.clockradio.player.Player;
+import de.briemla.clockradio.player.PlayerFactory;
 
 public class MainPanelTest extends GuiTest {
 
@@ -40,21 +41,26 @@ public class MainPanelTest extends GuiTest {
         verify(timeProvider, times(2)).timeProperty();
         verify(timeProvider).nextMinute();
         verifyNoMoreInteractions(timeProvider);
-        verifyZeroInteractions(alarmFactory);
+        verify(alarmFactory).create();
+        verifyNoMoreInteractions(alarmFactory);
     }
 
     @Override
     protected Parent getRootNode() {
         player = mock(Player.class);
         trigger = mock(Trigger.class);
-        alarmFactory = mock(AlarmFactory.class);
         timeProvider = mock(TimeProvider.class);
-        storage = mock(AlarmStorage.class);
 
         LocalTime now = LocalTime.of(0, 0);
         time = new SimpleObjectProperty<>(now);
         when(timeProvider.timeProperty()).thenReturn(time);
         when(timeProvider.nextMinute()).thenReturn(now.plus(1, MINUTES));
+        alarmFactory = mock(AlarmFactory.class);
+        storage = mock(AlarmStorage.class);
+        PlayerFactory playerFactory = mock(PlayerFactory.class);
+        Alarm alarm = new Alarm(playerFactory, timeProvider, () -> {});
+        when(alarmFactory.create()).thenReturn(alarm);
+
         return new MainPanel(player, trigger, alarmFactory, timeProvider, storage);
     }
 }
