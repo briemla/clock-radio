dda33212224749d91c802d9f0e0c4e6272ffe49c
Lars Briem <briem.lars@googlemail.com>
2016-04-29 21:24:18 +0200

    Add matcher to check existence of file

--- clock-radio/src/test/java/de/briemla/utils/matcher/FileStorageMatcher.java
+++ clock-radio/src/test/java/de/briemla/utils/matcher/FileStorageMatcher.java
@@ -7,34 +7,47 @@ import org.hamcrest.Matcher;
 public class FileStorageMatcher {
 
     /**
-     * Tests whether a given file is empty or not.
+     * Tests whether a given {@link File} is empty or not.
      *
-     * @return true if file is empty
+     * @return <code>true</code> if {@link File} is empty
      */
     public static Matcher<File> isEmpty() {
         return new IsEmpty();
     }
 
     /**
-     * Tests whether a given file contains only the given line
+     * Tests whether a given {@link File} contains only the given line.
      *
      * @param line
-     *            which should be contained in the file
-     * @return true if the file only contains this line, false otherwise
+     *            which should be contained in the {@link File}
+     * @return <code>true</code> if the {@link File} only contains this line, <code>false</code>
+     *         otherwise
      */
     public static Matcher<File> containsSingleLine(String line) {
         return new ContainsSingleLine(line);
     }
 
     /**
-     * Tests whether a given file contains all given lines
+     * Tests whether a given {@link File} contains all given lines.
      *
      * @param lines
-     *            which should be contained in the file
-     * @return true if the file contains all given lines, false otherwise
+     *            which should be contained in the {@link File}
+     * @return <code>true</code> if the {@link File} contains all given lines, <code>false</code>
+     *         otherwise
      */
     public static Matcher<File> contains(String... lines) {
         return new Contains(lines);
+    }
+
+    /**
+     * Tests whether a {@link File} exists or not.
+     *
+     * @see File#exists()
+     *
+     * @return <code>true</code> if the {@link File} exists otherwise <code>false</code>
+     */
+    public static Matcher<File> exists() {
+        return new Exists();
     }
 
 }
