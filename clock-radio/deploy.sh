234baba85b6d753554d00731467e4058ba863a66
Lars Briem <briem.lars@googlemail.com>
2016-05-01 14:32:19 +0200

    Restart pi after deploy

--- clock-radio/deploy.sh
+++ clock-radio/deploy.sh
@@ -4,6 +4,10 @@ git pull
 
 echo "Building clock-radio using maven"
 mvn clean compile test assembly:single && { echo 'Successfully build clock-radio' ; } || { echo 'Building clock-radio failed' ; exit 1; }
+
 echo "Deploying artifacts to clock-radio.local"
 scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar clock-radio:/home/pi/dabpi_ctl/
+
+echo "Restarting clock radio software"
+ssh clock-radio ./restart-clock-radio.sh
  
