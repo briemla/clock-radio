c04cc1d6f07e0a9ed61b1dbd74e0e9676702b8c2
Lars Briem <briem.lars@googlemail.com>
2016-05-16 16:30:20 +0200

    Play error sound until stopped

--- clock-radio/src/main/java/de/briemla/clockradio/PlayableError.java
+++ clock-radio/src/main/java/de/briemla/clockradio/PlayableError.java
@@ -2,6 +2,7 @@ package de.briemla.clockradio;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import de.briemla.clockradio.player.Player;
 
@@ -11,22 +12,32 @@ public class PlayableError implements PlayableMedia {
 
     private final Logger logger;
 
+    private final AtomicBoolean playing;
+
     public PlayableError(Logger logger) {
         super();
         this.logger = logger;
+        playing = new AtomicBoolean(true);
     }
 
     @Override
     public void play(Player player) {
         try {
-            player.play(errorSound());
+            playUntilStopped(player);
         } catch (URISyntaxException exception) {
             logger.log("Could not play error sound", exception);
         }
     }
 
+    private void playUntilStopped(Player player) throws URISyntaxException {
+        while (playing.get()) {
+            player.play(errorSound());
+        }
+    }
+
     @Override
     public void stop(Player player) {
+        playing.set(false);
         player.stop();
     }
 
--- clock-radio/src/test/java/de/briemla/clockradio/PlayableErrorTest.java
+++ clock-radio/src/test/java/de/briemla/clockradio/PlayableErrorTest.java
@@ -1,16 +1,39 @@
 package de.briemla.clockradio;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.net.URI;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import de.briemla.clockradio.player.Player;
 
 public class PlayableErrorTest {
+
+    private final class StopAfterSeveralRetries implements Answer<Void> {
+        private final AtomicInteger invocations = new AtomicInteger(0);
+
+        public StopAfterSeveralRetries() {
+            super();
+        }
+
+        @Override
+        public Void answer(InvocationOnMock media) throws Throwable {
+            invocations.incrementAndGet();
+            if (5 <= invocations.get()) {
+                error.stop(player);
+            }
+            return null;
+        }
+    }
 
     private Player player;
     private PlayableError error;
@@ -24,12 +47,15 @@ public class PlayableErrorTest {
         error = new PlayableError(notNeeded);
     }
 
-    @Test
-    public void playsErrorMediaOnPlayer() throws Exception {
+    @Test(timeout = 1000)
+    public void playsErrorMediaUntilStopped() throws Exception {
+        Answer<Void> stopAfterSeveralCallsOfPlay = new StopAfterSeveralRetries();
+        doAnswer(stopAfterSeveralCallsOfPlay).when(player).play(any(URI.class));
+
         error.play(player);
 
         URI uri = PlayableError.class.getResource("ErrorSound.mp3").toURI();
-        verify(player).play(uri);
+        verify(player, times(5)).play(uri);
     }
 
     @Test
