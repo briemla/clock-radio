4fc6744e02ad3edec39be543aa3c682143ab9fa8
Lars Briem <briem.lars@googlemail.com>
2016-05-16 16:47:44 +0200

    Change path to deploy to on pi

--- clock-radio/deploy.sh
+++ clock-radio/deploy.sh
@@ -6,7 +6,7 @@ echo "Building clock-radio using maven"
 mvn clean compile test assembly:single && { echo 'Successfully build clock-radio' ; } || { echo 'Building clock-radio failed' ; exit 1; }
 
 echo "Deploying artifacts to clock-radio.local"
-scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar clock-radio:/home/pi/dabpi_ctl/
+scp target/clock-radio-0.0.1-SNAPSHOT-jar-with-dependencies.jar clock-radio:/home/pi/clock-radio/
 
 echo "Restarting clock radio software"
 ssh clock-radio ./restart-clock-radio.sh
