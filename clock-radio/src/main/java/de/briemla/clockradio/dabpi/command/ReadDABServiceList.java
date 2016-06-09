58c4b6819a27afbce67214fe9331f998f80629a6
Lars Briem <briem.lars@googlemail.com>
2016-06-09 20:52:54 +0200

    Refactor

--- clock-radio/src/main/java/de/briemla/clockradio/dabpi/command/ReadDABServiceList.java
+++ clock-radio/src/main/java/de/briemla/clockradio/dabpi/command/ReadDABServiceList.java
@@ -9,12 +9,20 @@ import de.briemla.clockradio.dabpi.result.DABServiceList;
 
 public class ReadDABServiceList extends BaseCommand<DABServiceList> {
 
+    private static final String startOfLine = "^";
+    private static final String endOfLine = "$";
+    private static final String separator = "[ ]+";
+    private static final String serviceNumber = "Num:" + separator + "([0-9]+)";
+    private static final String serviceId = "Service ID:" + separator + "([aAbBcCdDeEfF0-9]+)";
+    private static final String serviceName = "Service Name:" + separator + "(.+)";
+    private static final String componentId = "Component ID:" + separator + "([0-9]+)" + endOfLine;
+    private static final int ATTRIBUTES = 4;
     private static final int NUMBER = 1;
     private static final int ID = 2;
     private static final int NAME = 3;
     private static final String FILTER = "Num:";
-    private static final Pattern PATTERN = Pattern
-            .compile("^Num:[ ]+([0-9]+)[ ]+Service ID:[ ]+([aAbBcCdDeEfF0-9]+)[ ]+Service Name:[ ]+(.+)Component ID:[ ]+([0-9]+)$");
+    private static final Pattern PATTERN = Pattern.compile(startOfLine + serviceNumber + separator
+            + serviceId + separator + serviceName + separator + componentId);
 
     public ReadDABServiceList() {
         super("g");
@@ -23,20 +31,28 @@ public class ReadDABServiceList extends BaseCommand<DABServiceList> {
     @Override
     protected DABServiceList parseSpecialized(Output output) {
         DABServiceList dabServiceList = new DABServiceList();
-        output.standardAsStream().filter(line -> line.startsWith(FILTER)).forEach(line -> dabServiceList.add(split(line)));
+        output.standardAsStream()
+              .filter(line -> line.startsWith(FILTER))
+              .map(line -> toService(line))
+              .forEach(service -> dabServiceList.add(service));
         return dabServiceList;
     }
 
-    private static DABService split(String line) {
-        Matcher matcher = PATTERN.matcher(line);
-        if (!matcher.matches() || matcher.groupCount() < 4) {
-            throw new IllegalArgumentException("Incorrect line: " + line);
-        }
+    private static DABService toService(String line) {
+        Matcher matcher = match(line);
 
         Integer number = Integer.parseInt(matcher.group(NUMBER));
         String id = matcher.group(ID).trim();
         String name = matcher.group(NAME).trim();
         return new DABService(number, id, name);
+    }
+
+    private static Matcher match(String line) {
+        Matcher matcher = PATTERN.matcher(line);
+        if (!matcher.matches() || matcher.groupCount() < ATTRIBUTES) {
+            throw new IllegalArgumentException("Incorrect line: " + line);
+        }
+        return matcher;
     }
 
 }
